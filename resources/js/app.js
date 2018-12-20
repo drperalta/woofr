32fb1797dfec1dcd2cec9f040bdc63fda4aac936
drperalta <peraltadavidr@outlook.com>
2018-12-20 23:23:08 +0800

    element ui configured

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,8 +1,16 @@
 window.Vue = require('vue');
 
+import App from './components/App.vue'
 import router from './router/index'
 
-import App from './components/App.vue'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css'
+import 'element-ui/lib/theme-chalk/reset.css'
+import 'element-ui/lib/theme-chalk/display.css'
+import locale from 'element-ui/lib/locale/lang/en';
+
+Vue.use(ElementUI, { locale })
+Vue.use(ElementUI)
 
 const app = new Vue({
     el: '#app',
