8bdf00e1fe05d3757452bb96681bb73238561b1c
drperalta <peraltadavidr@outlook.com>
2018-12-21 21:42:22 +0800

    remove vuetify and element ui and fixed the components

--- resources/js/components/Home/Home.vue
+++ resources/js/components/Home/Home.vue
@@ -1,21 +1,6 @@
 <template>
     <div>
         <!-- NAVBAR -->
-        <el-menu class="navbar" mode="horizontal">
-            <el-menu-item index="1"><router-link to="/timeline">Home</router-link></el-menu-item>
-            <el-menu-item index="2"><router-link to="/notifications">Notifications</router-link></el-menu-item>
-            <el-menu-item index="3"><router-link to="/messages">Messages</router-link></el-menu-item>
-            
-            <h1 class="logo">
-                <img class="logo" src="../../../img/woofr-logo.png" alt="">
-            </h1>
-
-            <!-- <span class="items">
-                <el-autocomplete size="medium" class="inline-input" v-model="state2" :fetch-suggestions="querySearch" placeholder="Search Woofr" :trigger-on-focus="false" @select="handleSelect">
-                </el-autocomplete>
-                <img class="profile" src="../../../img/default-picture.png" alt="">
-            </span> -->
-        </el-menu>
 
         <div class="body">
             <router-view></router-view>
@@ -27,69 +12,13 @@
 export default {
     data(){
         return{
-            links: [],
-            state1: '',
-            state2: ''
+
         }
-        
-    },
-    methods: {
-        querySearch(queryString, cb) {
-            var links = this.links;
-            var results = queryString ? links.filter(this.createFilter(queryString)) : links;
-            // call callback function to return suggestions
-            cb(results);
-        },
-        createFilter(queryString) {
-            return (link) => {
-            return (link.value.toLowerCase().indexOf(queryString.toLowerCase()) === 0);
-            };
-        },
-        loadAll() {
-            return [
-            { "value": "vue", "link": "https://github.com/vuejs/vue" },
-            { "value": "element", "link": "https://github.com/ElemeFE/element" },
-            { "value": "cooking", "link": "https://github.com/ElemeFE/cooking" },
-            { "value": "mint-ui", "link": "https://github.com/ElemeFE/mint-ui" },
-            { "value": "vuex", "link": "https://github.com/vuejs/vuex" },
-            { "value": "vue-router", "link": "https://github.com/vuejs/vue-router" },
-            { "value": "babel", "link": "https://github.com/babel/babel" }
-            ];
-        },
-        handleSelect(item) {
-            console.log(item);
-        }
-    },
-    mounted() {
-            this.links = this.loadAll();
-        }
+
+    }
 }
 </script>
 
 <style scoped>
-.searchBar{
-    width: 250px;
-}
-.logo{
-    width: 40px;
-    margin-left: auto;
-    margin-right: auto;
-    margin-top: 5px;
-    
-}
-.items{
-    float: right;
-    max-height: 60px;
-    margin-top: 10px;
-}
-.navbar{
-    padding-left: 15%;
-    padding-right: 15%;
-    min-width: 1000px;
-    max-height: 60px;
-}
-.body{
-    padding-left: 15%;
-    padding-right: 15%;
-}
+
 </style>
--- resources/js/components/Index/Partials/SignUp.vue
+++ resources/js/components/Index/Partials/SignUp.vue
@@ -1,22 +1,6 @@
 <template>
     <div class="content">
         <p class="name">SIGN UP</p>
-        
-        <v-form>
-            <div>
-                <el-input class="el-input" placeholder="Name" v-model="SignUpDetails.full_name"></el-input>
-                <el-input class="el-input" placeholder="Username" v-model="SignUpDetails.user_name"></el-input>
-                <el-input class="el-input" placeholder="Email" v-model="SignUpDetails.user_email"></el-input>
-                <el-input class="el-input" placeholder="Password" type="password" v-model="SignUpDetails.password"></el-input>
-                <el-input class="el-input" placeholder="Confirm Password" type="password" v-model="SignUpDetails.confirm_password"></el-input>
-            </div>
-            <div class="bottom">
-                <v-btn outline class="signUpButton" type="submit">Sign Up</v-btn>
-                <router-link to="/login">
-                        <a style="color: #8fb9a8" >Already a member? Sign In</a>
-                </router-link>
-            </div>
-        </v-form>
     </div>
 </template>
 
@@ -37,26 +21,5 @@ export default {
 </script>
 
 <style scoped>
-.content{
-    display: inline;
-}
-.name{
-    font-size: 14px;
-    font-weight: 300;
-    color: #765d69;
-    text-align: center;
-}
-.el-input{
-    padding: 4px;
-}
-.signUpButton{
-    width: 100%;
-    color: #765d69;
-    margin: 0;
-    margin-bottom: 10px;
-    padding: 4px;
-    border-color: #765d69;
-    opacity: 0.7;
-    margin-top: 30px;
-}
+
 </style>
