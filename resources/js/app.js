b78df6bc7112a94b1f4116ecb38a862c8dc9e081
drperalta <peraltadavidr@outlook.com>
2018-12-21 22:31:19 +0800

    configured iview

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,6 +3,11 @@ window.Vue = require('vue');
 import App from './components/App.vue'
 import router from './router/index'
 
+import iView from 'iview';
+import 'iview/dist/styles/iview.css';
+import locale from 'iview/dist/locale/en-US';
+
+Vue.use(iView, { locale });
 
 const app = new Vue({
     el: '#app',
