e293d0092a6bc539929ae774aec41efdb81ec32a
drperalta <peraltadavidr@outlook.com>
2018-12-23 17:37:00 +0800

    created a reset password component

---
+++ resources/js/components/Index/Partials/ResetPassword.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="content">
+        <p class="page-name">RESET PASSWORD</p>
+        <Form inline>
+            <!-- TOP -->
+            <Input class="input" placeholder="Email" v-model="LoginDetails.user_email"/>
+            <!-- BOTTOM -->
+            <Button class="button">Send Link</Button>
+            <router-link to="/login">Nevermind, I got it.</router-link>
+        </Form>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            UserDetails:{
+                user_email: ''
+            }
+        }
+    }
+}
+</script>
+
+<style scoped>
+.page-name{
+    margin-bottom: 20px;
+}
+.input{
+    width: 100%;
+    margin: 4px 0px;
+}
+.button{
+    width: 100%;
+    margin-bottom: 8px;
+}
+</style>
