790423856a282cd7974b07120edb0c21b38dba06
David Peralta <peraltadavidr@outlook.com>
2018-12-28 13:49:24 +0800

    added trending partial in notif page

--- resources/js/components/Home/Pages/Notifications.vue
+++ resources/js/components/Home/Pages/Notifications.vue
@@ -1,11 +1,25 @@
 <template>
-    <div>
-        NOTIFICATIONS
+    <div class="content">
+        <!-- TRENDING BOX -->
+        <div class="box left">
+            <Trending></Trending>
+        </div>
+        <!-- NOTIFICATIONS BOX -->
+        <div class="box middle">
+
+        </div>
+        <!--BOX -->
+        <div class="box right">
+
+        </div>
     </div>
 </template>
 
 <script>
+import Trending from '../Partials/Trending'
+
 export default {
+    components: {Trending},
     data(){
         return{
 
@@ -18,5 +32,35 @@ export default {
 </script>
 
 <style scoped>
+.content{
+    margin-top: 7px;
+}
+.box{
+    margin: 5px;
+}
+.left{
+    max-width: 290px;
+    min-width: 290px;
+    float: left;
 
+    border-width: 5px;
+    border-style: solid;
+    border-color: #FFFFFF;
+    border-radius: 5px;
+}
+.middle{
+    min-width: 590px;
+    max-width: 590px;
+    float: left;
+}
+.right{
+    min-width: 290px;
+    max-width: 290px;
+    float: left;
+
+    border-width: 5px;
+    border-style: solid;
+    border-color: #FFFFFF;
+    border-radius: 5px;
+}
 </style>
