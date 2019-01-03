d5adbf346832045ad80711924fc63059b08f452e
David Peralta <peraltadavidr@outlook.com>
2019-01-03 16:02:35 +0800

    added new partial component in profile

---
+++ resources/js/components/Home/Partials/Profile/Woofs.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        WOOFS
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
