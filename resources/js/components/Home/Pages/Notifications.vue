f10ba19a1ff96da59570b8ff6a98ce782f6f7244
drperalta <peraltadavidr@outlook.com>
2019-01-07 02:10:40 +0800

    fixed some ui in notifications page

--- resources/js/components/Home/Pages/Notifications.vue
+++ resources/js/components/Home/Pages/Notifications.vue
@@ -1,18 +1,18 @@
 <template>
     <div class="content">
         <!-- TRENDING BOX -->
-        <div class="box left">
+        <div class="box side-box left woofr-border">
             <Trending></Trending>
         </div>
         <!-- NOTIFICATIONS BOX -->
-        <div class="box middle">
+        <div class="box middle-box woofr-border left white">
             <div class="notifications-header">
                 <ButtonGroup>
                     <Button size="large" to="/notifications">All</Button>
                     <Button size="large" to="/notifications/mentions">Mentions</Button>
                 </ButtonGroup>
             </div>
-            <Divider style="margin: 5px 0px"/>
+            <Divider class="idivider"/>
             <div class="notifications-body">
                 <router-view></router-view>
             </div>
@@ -42,41 +42,7 @@ export default {
 
 <style scoped>
 .content{
-    margin-top: 7px;
-}
-.box{
-    margin: 5px;
-}
-.left{
-    max-width: 290px;
-    min-width: 290px;
-    float: left;
-
-    border-width: 5px;
-    border-style: solid;
-    border-color: #FFFFFF;
-    border-radius: 5px;
-}
-.middle{
-    min-width: 590px;
-    max-width: 590px;
-    float: left;
-    background-color: white;
-
-    border-width: 5px;
-    border-style: solid;
-    border-color: #FFFFFF;
-    border-radius: 5px;
-}
-.right{
-    min-width: 290px;
-    max-width: 290px;
-    float: left;
-
-    border-width: 5px;
-    border-style: solid;
-    border-color: #FFFFFF;
-    border-radius: 5px;
+    margin-top: 5px;
 }
 .notifications-header{
     padding: 10px;
