683fc58a74456deefc4cd83fc3e040ef500c3d30
David Peralta <peraltadavidr@outlook.com>
2019-01-07 10:06:51 +0800

    created primary and password component as partial for settings

---
+++ resources/js/components/Home/Partials/Settings/Password.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="content left">
+        <Form label-position="right" :label-width="110  ">
+            <FormItem label="Current Password">
+                <Input class="input" v-model="UserDetails.password" size="large"/>
+            </FormItem>
+            <FormItem label="New Password">
+                <Input class="input" v-model="UserDetails.new_password" size="large"/>
+            </FormItem>
+            <FormItem label="Confirm Password">
+                <Input class="input" v-model="UserDetails.confirm_current_pasword" size="large"/>
+            </FormItem>
+        </Form>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            PasswordDetails:{
+                password: '',
+                new_password: '',
+                confirm_new_password: '',
+            }
+        }
+    }
+}
+</script>
+
+<style scoped>
+.content{
+    width: 450px;
+}
+.input{
+    width: 250px;
+    margin: 5px;
+    display: block;
+}
+.form-item{
+    margin-bottom: 10px !important;
+}
+</style>
---
+++ resources/js/components/Home/Partials/Settings/Primary.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="content left">
+            <Form label-position="right" :label-width="100">
+                <FormItem class="form-item" label="Full Name">
+                    <Input class="input" v-model="UserDetails.full_name" placeholder="Enter your Full Name here" size="large"/>
+                </FormItem>
+                <FormItem class="form-item" label="Username">
+                    <Input class="input" v-model="UserDetails.user_name" placeholder="Enter your Username here" size="large"/>
+                    <span style="margin-left: 7px; color: grey">https://woofr.com/</span>{{UserDetails.user_name}}
+                </FormItem>
+                <FormItem class="form-item" label="Email">
+                    <Input class="input" v-model="UserDetails.user_email" placeholder="Enter your Email her" size="large"/>
+                    <span style="margin-left: 7px; color: grey;">Email will not be publicly displayed.</span>
+                </FormItem>
+            </Form>
+        </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            PrimaryDetails:{
+                full_name: 'David Peralta',
+                user_name: 'IamDavidMe',
+                user_email: 'peraltadavidr@outlook.com',
+            }
+        }
+    }
+}
+</script>
+
+<style scoped>
+.content{
+    width: 450px;
+}
+.input{
+    width: 250px;
+    margin: 5px;
+    display: block;
+}
+.form-item{
+    margin-bottom: 10px !important;
+}
+</style>
