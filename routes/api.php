eb94f19f7381a1b4abd38417e21a6a3f755a46a6
drperalta <peraltadavidr@outlook.com>
2019-01-11 02:12:29 +0800

    remove "me" api

--- routes/api.php
+++ routes/api.php
@@ -24,5 +24,4 @@ Route::group([ 'prefix' => 'auth' ], function ()
     Route::post('signup', 'AuthController@signup');
     Route::post('login', 'AuthController@login');
     Route::post('logout', 'AuthController@logout');
-    Route::get('me', 'AuthController@me');
 });
