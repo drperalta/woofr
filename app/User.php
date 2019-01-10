85cae3bada82ac96c9f7f847c12a6c663f781bde
David Peralta <peraltadavidr@outlook.com>
2019-01-10 16:18:52 +0800

    remove password identifier in user model

--- app/User.php
+++ app/User.php
@@ -11,21 +11,27 @@ class User extends Authenticatable implements JWTSubject
 {   
     use Notifiable;
 
+    // Rest omitted for brevity
+
+    /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     *
+     * @return mixed
+     */
     public function getJWTIdentifier()
     {
         return $this->getKey();
     }
 
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
     public function getJWTCustomClaims()
     {
         return [];
-    }
-    public function setPasswordAttribute($password)
-    {
-        if ( !empty($password) ) {
-            $this->attributes['password'] = bcrypt($password);
-        }
-    }    
+    } 
     
     /**
      * The attributes that are mass assignable.
