e49287c7227a50eaaae579c2d130e6b3e14878f4
David Peralta <peraltadavidr@outlook.com>
2019-01-14 10:09:42 +0800

    fixed he notifs in reset password

--- resources/js/api/reset.js
+++ resources/js/api/reset.js
@@ -4,17 +4,33 @@ export default function(Vue){
         create(context, data){
             axios.post('/api/password/create', data)
             .then(response => {
-                console.log(response)
+                //clear the inputs
+                context.UserDetails = '';
+                
+                context.success.success = true;
+                context.success.message = response.data.message;
             }).catch(error => {
-                console.log(error)
+                context.error.error = true;
+
+                var errorArray = Object.values(error.response.data.errors);
+                context.error.message = errorArray[0][0]
             })
         },
+
         reset(context, data){
             axios.post('/api/password/reset', data)
             .then(response => {
-                console.log(response)
+                //clear the inputs
+                context.ResetPasswordDetails.password = '';
+                context.ResetPasswordDetails.confirm_password = '';
+
+                context.success.success = true;
+                context.success.message = response.data.message;
             }).catch(error => {
-                console.log(error)
+                context.error.error = true;
+
+                var errorArray = Object.values(error.response.data.errors);
+                context.error.message = errorArray[0][0]
             })
         }
 
