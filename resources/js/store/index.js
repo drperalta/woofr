cfa8f1d5bb0af2c92f1816e063a0f30690599675
David Peralta <peraltadavidr@outlook.com>
2019-01-16 17:38:43 +0800

    added visiteduserdata in store

--- resources/js/store/index.js
+++ resources/js/store/index.js
@@ -11,14 +11,20 @@ export default new Vuex.Store({
         woofList: [],
         commentList: [],
         usersList: [],
+        visitedUserData: []
 
     },
 
     getters: {
         UserData: state => {
             return  state.userDetails;
+        },
+        visitedData: state => {
+            return state.visitedUserData;
+        },
+        UserList: state => {
+            return state.usersList;
         }
-
     },
 
     mutations:{
@@ -30,12 +36,24 @@ export default new Vuex.Store({
         },
         SET_USER: (state, data) => {
             state.userDetails = data
+        },
+        SET_VISITED_USER: (state, data) => {
+            state.visitedUserData = data
+        },
+        SET_USER_LIST: (state, data) => {
+            state.usersList = data
         }
     },
 
     actions:{
         GET_USER({commit}) {
             commit('SET_USER');
+        },
+        GET_VISITED_USER({commit}) {
+            commit('SET_VISITED_USER');
+        },
+        GET_USER_LIST({commit}){
+            commit('SET_USER_LIST');
         }
     }
 
